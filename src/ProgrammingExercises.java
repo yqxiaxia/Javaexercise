d62fb85bcba0e9b0092a5435cba92bfe502da0b3
yqxia <yqxia@thoughtworks.com>
2017-09-09 19:26:36 +0800

    push code

--- src/ProgrammingExercises.java
+++ src/ProgrammingExercises.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
-
 public class ProgrammingExercises {
+    public static void main(String[] args) {
+        ProgrammingExercises exercises = new ProgrammingExercises();
+        System.out.println("===one Asterisk===");
+        exercises.oneAster();
+        System.out.println("===line Asterisk===");
+        exercises.oneLineAster(8);
+        System.out.println("===multiple line Asterisk===");
+        exercises.mutliLineAster(3);
+        System.out.println("===triangle Asterisk===");
+        exercises.triangle(6);
+    }
+
+    private void oneAster(){
+        System.out.println("*");
+    }
+
+    private void oneLineAster(int num){
+        for (int i = 0; i < num; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    private void mutliLineAster(int line){
+        for (int i = 0; i < line; i++) {
+            System.out.println("*");
+        }
+    }
+
+    private void triangle(int line){
+        for (int i = 0; i < line; i++) {
+            oneLineAster(i+1);
+        }
+    }
 }
--- src/firstRes.java
+++ src/firstRes.java
@@ -1,7 +1,12 @@
-public class test {
+public class firstRes {
 
-    public void printNum() {
-        int beerNum = 99;
+    public static void main(String[] args) {
+        firstRes firstRes = new firstRes();
+        firstRes.printNum();
+    }
+
+    private void printNum() {
+        int beerNum = 5;
         while (beerNum > 0) {
             beerNum = beerNum - 1;
             System.out.println(beerNum);

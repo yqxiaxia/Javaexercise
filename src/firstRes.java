57f6627bd14e4a31296fa8f501780caa8564d8ce
yqxia <yqxia@thoughtworks.com>
2017-09-09 18:51:07 +0800

    add programmingExercise

---
+++ src/firstRes.java
@@ -0,0 +1,10 @@
+public class test {
+
+    public void printNum() {
+        int beerNum = 99;
+        while (beerNum > 0) {
+            beerNum = beerNum - 1;
+            System.out.println(beerNum);
+        }
+    }
+}
